ed6e9587e83b741bca88464bb8f60356e0b692fd
Kelvin Henrique Ferreira <94548044+kelvinhenri99@users.noreply.github.com>
2021-12-10 08:12:25 -0300

    Importação do código

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE HTML>
+
+<HTML>
+
+<HEAD>
+	<link rel="stylesheet" href="Estilos/css.css" type="text/css"/>
+	<title>CALCULADORA EM PHP</title>
+</HEAD>
+
+<BODY>
+
+	<div class="quadrado1">
+		<div align="center" class="quadrado2">CALCULADORA SIMPLES</div>
+		
+		<form method="POST" action="index.php">
+			<div class="quadrado3">
+					<textarea class="estilo2"><?php
+						$n1 = 1;
+							echo $n1 = ?  + $n1;
+					?></textarea>
+			</div>
+
+			<div class="quadrado4"><input type="reset" value="apagar" class="estilo1"></input></div>		
+			<div class="quadrado5"><input type="button" value="7" class="estilo1"></input></div>
+			<div class="quadrado5"><input type="button" value="4" class="estilo1"></input></div>
+			<div class="quadrado5"><input type="button" value="1" class="estilo1"></input></div>
+			<div class="quadrado6"><input type="button" value="0" class="estilo1"></input></div>
+			<div class="quadrado7"><input type="button" value="8" class="estilo1"></input></div>
+			<div class="quadrado8"><input type="button" value="5" class="estilo1"></input></div>
+			<div class="quadrado8"><input type="button" value="2" class="estilo1"></input></div>
+			<div class="quadrado9"><input type="button" value="/" class="estilo1"></input></div>
+			<div class="quadrado10"><input type="button" value="9" class="estilo1"></input></div>
+			<div class="quadrado10"><input type="button" value="6" class="estilo1"></input></div>
+			<div class="quadrado10"><input type="button" value="3" class="estilo1"></input></div>
+			<div class="quadrado10"><input type="button" value="," class="estilo1"></input></div>
+			<div class="quadrado11"><input type="submit" value="=" class="estilo1"></input></div>
+			<div class="quadrado12"><input type="button" value="+" class="estilo1"></input></div>
+			<div class="quadrado13"><input type="button" value="-" class="estilo1"></input></div>
+			<div class="quadrado14"><input type="button" value="*" class="estilo1"></input></div>
+		</form>
+		
+	</div>
+
+</BODY>
+
+</HTML>
